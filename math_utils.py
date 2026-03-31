c1c7b803c890e2f3a837dbe0a6e952ece1b519d6
Calvin Bonomo <cal.bonomo@gmail.com>
2026-03-31 12:06:55 -0600

    Added utility function to compute 3d rotation matrix

---
+++ math_utils.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+def quat_to_mat(q: np.ndarray) -> np.ndarray:
+    """Convert a quaternion to a 3x3 rotation matrix.
+
+    Args:
+        q: Quaternion as [x, y, z, w].
+
+    Returns:
+        3x3 rotation matrix.
+    """
+    x, y, z, w = q / np.linalg.norm(q)
+
+    return np.array([
+        [1 - 2*(y*y + z*z),     2*(x*y - w*z),     2*(x*z + w*y)],
+        [    2*(x*y + w*z), 1 - 2*(x*x + z*z),     2*(y*z - w*x)],
+        [    2*(x*z - w*y),     2*(y*z + w*x), 1 - 2*(x*x + y*y)],
+    ])
