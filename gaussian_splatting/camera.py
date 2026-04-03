ffcc2641ce4ec06c8d0d6f7b013e13761428dcd2
Calvin Bonomo <cal.bonomo@gmail.com>
2026-04-03 10:21:13 -0600

    Finished camera projection matrix function

--- gaussian_splatting/camera.py
+++ gaussian_splatting/camera.py
@@ -9,12 +9,24 @@ class Camera:
         self.image = image
 
     def _create_T3D(self, rotation: np.ndarray, translation: np.ndarray, focal: np.ndarray):
-        # Create viewing transform (W)
-        transform = np.identity(4)
+        """
+        Creates the 3D transformation described in the EWA splatting paper.
 
+        Parameters:
+            rotation: A quaternion in the form [x, y, z, w]
+            translation: A 3-vector describing the position of the camera
+            focal: A 2-vector describing the focal-length of the camera
+
+        Returns:
+            A 2x3 matrix
+        """
+        # Create viewing transform (W)
         rot_mat = quat_to_mat(rotation)
-        transform[:3, :3] = rot_mat
-        transform[:3, 3] = translation.T
         
         # Create projection matrix (J)
-        
+        u2_sqrd = np.pow(translation[2], 2)
+        projection_mat = [
+                [focal[0] / translation[2], 0, -focal[0] * translation[0] / u2_sqrd],
+                [0, focal[1] / translation[2], -focal[1] * translation[1] / u2_sqrd]]
+
+        return projection_mat @ rot_mat
