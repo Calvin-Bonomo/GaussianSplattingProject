6908afaf8802dde0761cb1b7f754980cb8b1ed22
Calvin Bonomo <cal.bonomo@gmail.com>
2026-04-06 17:38:08 -0600

    Finished writing GaussianModel init

--- gaussian_splatting/gaussian_model.py
+++ gaussian_splatting/gaussian_model.py
@@ -1,13 +1,21 @@
-from torch import nn, optim
+import torch
+from torch import nn, optim, from_numpy
+import numpy as np
 
 
 class GaussianModel:
-    def __init__(self):
+    def __init__(self, xyz: np.ndarray, rgb: np.ndarray):
+        # Prepare data
+        num_points = xyz.shape[0]
+        rotations = torch.zeros(num_points, 4, dtype=torch.float32)
+        rotations[:, 0] = 1.0
+
         # Setup the gaussian parameters
-        self.mean = nn.Parameter()
-        self.scale = nn.Parameter()
-        self.rotation = nn.Parameter()
-        self.color = nn.Parameter()
+        self.mean = nn.Parameter(from_numpy(xyz).float().cuda().requires_grad_(True))
+        self.scale = nn.Parameter(torch.eye(3).unsqueeze(0).repeat(num_points, 1, 1).float().cuda().requires_grad_(True))
+        self.rotation = nn.Parameter(rotations.cuda().requires_grad_(True))
+        self.color = nn.Parameter(from_numpy(rgb).float().cuda().requires_grad_(True))
+        self.opacity = nn.Parameter(0.1 * torch.ones(num_points))
 
         # Setup the optimizer
         self.optimizer = optim.adam.Adam() # TODO: Setup learning rates
